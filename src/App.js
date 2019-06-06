79a6b9fdb9f18349184a1dbeaa8601c3a54128f1
michealxie <yinxie@xiaohongshu.com>
2019-06-06 19:51:33 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react';
+import { fromEvent, merge, interval, Observable, from, of, Subject } from 'rxjs';
+import { tap, map, filter, take, mergeAll, mergeMap } from 'rxjs/operators';
 import './App.css';
+import { scan } from 'rxjs/operators';
+import { audit } from 'rxjs/operators'
+
+// const clicks = fromEvent(document, 'click');
+// const result = clicks.pipe(audit(ev => interval(1000)));
+// result.subscribe(x => console.log(x));
+
+// fromEvent(document, 'click') // 数据源, 也就是 observable, 使用迭代器模式处理流的信息, 相当于帮你手动调用了 next(e)
+//   .pipe(scan(count => count + 1, 0)) // 流, 一般为操作符
+//   .subscribe(count => console.log(`Clicked ${count} times`)); // subscribe 里的函数即为 observer 对数据源发出的信息做出反应
+
+
+// const consoler = new Observable(subscribe => {
+//   subscribe.next(2)
+//   subscribe.next(2)
+//   subscribe.next(2)
+//   subscribe.next(2)
+//   subscribe.next(2)
+//   subscribe.error(3)
+// }).subscribe(console.log, console.log)
+
+// 另一种创建方法: 操作符返回一个函数, 参数是 Observable, 一般不这么用
+// map(x => x * x)(of(1, 2, 3)).subscribe((v) => console.log(`value: ${v}`));
+
+
+// const clicks = fromEvent(document, 'click');
+// const higherOrder = clicks.pipe(
+//   map((ev) => interval(100).pipe(take(5))),
+//   mergeAll(),
+// ).subscribe(console.log)
+// const result = higherOrder.pipe(exhaust());
+// result.subscribe(x => console.log(x));
 
 function App() {
+  const [todoItems, setTodoItems] = useState([])
+  const $inputRef = useRef(null)
+  const $addBtnRef = useRef(null)
+  const $todoItems = useRef(null)
+  const $todoItem = useRef(null)
+  const itemClicked$ = new Subject()
+  itemClicked$.subscribe(console.log)
+
+  useEffect(() => {
+    const inputEnter$ = fromEvent($inputRef.current, 'keydown').pipe(
+      filter(e => (e.key === 'Enter'))
+    )
+    const addClicked$ = fromEvent($addBtnRef.current, 'click')
+
+    const todoAdd$ = merge(addClicked$, inputEnter$)
+      .pipe(
+        map(() => $inputRef.current.value),
+        filter(name => name),
+        map(name => {
+          return {
+            name,
+            done: false,
+            id: new Date().valueOf(),
+          }
+        }),
+        scan((acc, todoItem) => {
+          return acc.concat(todoItem)
+        }, todoItems),
+        tap(updatedTodoItems => {
+          setTodoItems(updatedTodoItems)
+          $inputRef.current.value = ''
+          return $todoItems.current
+        }),
+        map($todoItems => {
+          console.log($todoItems);
+        })
+      )
+
+
+    todoAdd$.subscribe()
+
+
+    // const todoClicked$ = fromEvent($todoItems.current, 'click').pipe(
+    //   filter(e => e.target.nodeName.toUpperCase() === "LI"),
+    //   map(e => e.target),
+    // )
+
+    // todoClicked$.subscribe()
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-area">
+        <input type="text" className="todo-val" ref={$inputRef}/>
+        <div className="add-btn" ref={$addBtnRef}>添加</div>
+      </div>
+      <ul className="todo-items" ref={$todoItems}>
+        {
+          todoItems.map(item => (
+            <li className={`item ${item.done ? 'done' : ''}`} key={item.id} ref={$todoItem} onClick={e => { itemClicked$.next(e) }}>
+              {item.name}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
